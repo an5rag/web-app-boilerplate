e6d141392867d9cc93d00f5ba1f213167ec2c9f9
Anurag Choudhary <choudharya@nextcapital.com>
2016-10-25 21:28:27 -0500

    add example to show one controller change the rootscope and another watching it

--- js/controllers/input-controller.js
+++ js/controllers/input-controller.js
@@ -1,6 +1,10 @@
 angular.module('zenvisageControllers')
 
-    .controller('InputController', ['$scope','$state', function ($scope, $state) {
+    .controller('InputController', ['$scope', '$state', '$rootScope', function ($scope, $state, $rootScope) {
         $scope.stuff = "This is an input element!";
-        $scope.state = $state;
+        $rootScope.shared = {value:"Something to begin with!"};
+        setTimeout(function () {
+            $rootScope.shared = {value:"The input controller just changed this"};
+            $rootScope.$digest();
+        }, 3000);
     }]);
--- js/controllers/output-controller.js
+++ js/controllers/output-controller.js
@@ -1,5 +1,9 @@
 angular.module('zenvisageControllers')
 
-    .controller('OutputController', ['$scope', function ($scope) {
+    .controller('OutputController', ['$scope','$rootScope', function ($scope,$rootScope) {
         $scope.stuff = "This is an output element!";
+        $rootScope.$watch('shared', function() {
+            $scope.temp = $rootScope.shared.value;
+            console.log($rootScope.shared.value);
+        }, true);
     }]);
